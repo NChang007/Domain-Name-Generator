97f454bfae409a0f59af038f105ca747f6a4182a
NChang007 <nelsonc924@gmail.com>
2022-05-20 23:24:17 +0000

    It think im done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [".com", ".net", ".me", ".rip"];
+
+window.onload = () => {
   //write your code here
+  let randText = document.getElementById("pepers");
+  function genSentence() {
+    let pronounRan = Math.floor(Math.random() * pronoun.length);
+    let adjRan = Math.floor(Math.random() * adj.length);
+    let nounRan = Math.floor(Math.random() * noun.length);
+    let domRan = Math.floor(Math.random() * dom.length);
+
+    randText.innerHTML =
+      pronoun[pronounRan] + adj[adjRan] + noun[nounRan] + dom[domRan];
+  }
+  genSentence();
+  let newButton = document.getElementById("refreshButton");
+  newButton.onclick = function() {
+    genSentence();
+  };
   console.log("Hello Rigo from the console!");
 };
